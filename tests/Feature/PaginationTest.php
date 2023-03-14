5559c52efa3c1e217082088626d8da05387e3dd8
Maxwell Maragia <maxwellm@ringier.co.za>
2023-03-14 10:50:08 +0300

    Results in page two have a link to page one and the link does not have page parameter

--- tests/Feature/PaginationTest.php
+++ tests/Feature/PaginationTest.php
@@ -3,7 +3,7 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Symfony\Component\DomCrawler\Crawler;
 use Tests\TestCase;
 
 class PaginationTest extends TestCase
@@ -15,5 +15,14 @@ class PaginationTest extends TestCase
     {
         $response = $this->get('/');
         $response->assertStatus(200)->assertSee('?page=2');
+    }
+
+    public function test_that_results_page_two_has_a_link_to_page_one_and_that_the_link_does_not_have_page_parameter(): void{
+        $response = $this->get('/?page=2');
+        $crawler = new Crawler($response->getContent());
+        $link = $crawler->filter('nav[aria-label="Pagination"] a:contains("1"):not(:contains("~[0-9]")):not(:contains("~[a-z]")):not(:contains("~[A-Z]"))')->first();
+        $this->assertNotNull($link, 'Link not found');
+        $url = $link->attr('href');
+        $this->assertStringNotContainsString('page=1', $url, 'Link contains "page" parameter');
     }
 }
