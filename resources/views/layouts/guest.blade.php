78966fd0130fed3647dade987d1c17ae95b5edc9
Maxwell Maragia <maxwellm@ringier.co.za>
2023-03-14 17:01:10 +0300

    Add next and prev section to guest template layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,6 +16,8 @@
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
         <link rel="manifest" href="/site.webmanifest">
         <link rel="canonical" href="{{ request()->fullUrl() }}">
+        @section('prev_and_next_links')
+        @show
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
